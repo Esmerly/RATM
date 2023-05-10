a8d1d5f91ae50373136af6c700cf1ccf51772e4e
ArletteNaom <125988488+ArletteNaom@users.noreply.github.com>
2023-05-10 15:37:18 -0400

    Add files via upload

---
+++ vent.js
@@ -0,0 +1,6 @@
+let close_button = document.getElementById('close-button');
+close_button.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.getElementById("window-notice").style.display = "none";
+});
+Footer
